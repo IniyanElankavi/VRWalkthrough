209faf1f02b59537dade0ec128f16fa995ff3aa6
Iniyan <iniyane@ggsinc.com>
2022-03-17 21:22:00 +0530

    Added Light and Teleport Interactions

---
+++ VRWalkthrough/Assets/Scripts/Interactions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactions : MonoBehaviour
+{
+    public Light[] _lights;
+    bool lightSwitch = false;
+
+    private void Start()
+    {
+        _lights = GameObject.Find("Interior").GetComponentsInChildren<Light>();
+    }
+
+    public void TurnOnOffLight()
+    {
+        lightSwitch =! lightSwitch;
+
+        for (int i = 0; i < _lights.Length; i++)
+        {
+            if (lightSwitch)
+            {
+                _lights[i].enabled = false;
+            }
+            else
+            {
+                _lights[i].enabled = true;
+            }
+        }
+    }
+
+}
---
+++ VRWalkthrough/Assets/Scripts/TeleportController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportController : MonoBehaviour
+{
+    public GameObject Player;
+
+    public void Teleport()
+    {
+        Player.transform.position = new Vector3(transform.position.x, transform.position.y+1.3f,transform.position.z);
+    }
+}
